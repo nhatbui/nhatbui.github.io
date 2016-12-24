1f0dda467d9635a8fa67df656d3d2ad43b82a009
Nhat Bui <nhatbui@Nhats-MBP.fios-router.home>
2016-12-24 11:05:24 -0800

    New udpate

--- js/libhrc/libhrc.bundle.js
+++ js/libhrc/libhrc.bundle.js
@@ -31,8 +31,13 @@ var findAllIndicesRegex = function(str, re) {
   return indices;
 };
 
+var nullOutChars = function(str, start, end, nullChar) {
+  return str.slice(0, start) + nullChar.repeat(end - start) + str.slice(end);
+}
+
 module.exports.findAllIndices = findAllIndices;
 module.exports.findAllIndicesRegex = findAllIndicesRegex;
+module.exports.nullOutChars = nullOutChars;
 
 },{}],2:[function(require,module,exports){
 var common = require('../lib/common.js');
@@ -121,16 +126,6 @@ var LCPArray = function(str, splitOn) {
     }
   }
 
-  lcp_array.sort(function(a, b) {
-    if (a.length < b.length) {
-      return -1;
-    } else if (a.length > b.length) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
-
   return lcp_array;
 };
 
@@ -159,9 +154,9 @@ var findRuns = function(pattern, doc, delimiter) {
   // list of tuples where each tuple is the start of the run and the number
   // of times that run is repeated.
   if (matches.length === 0) {
-    return doc;
+    return [];
   } else if (matches.length === 1) {
-    return [{start: matches[0], count: 1}];
+    return [{start: matches[0], count: 1, pattern: pattern}];
   } else {
     // collect "runs"
     var runs = [];
@@ -175,7 +170,7 @@ var findRuns = function(pattern, doc, delimiter) {
         runCount++;
       } else {
         // Log the run.
-        runs.push({start: runStart, count: runCount});
+        runs.push({start: runStart, count: runCount, pattern: pattern});
 
         // Restart run counting
         runStart = matches[i];
@@ -187,7 +182,7 @@ var findRuns = function(pattern, doc, delimiter) {
     //    - If there's one run, it still hasn't been logged.
     // - for n number of runs where n > 1, the last is not printed so we better
     //   make sure we handle it outside of this for loop.
-    runs.push({start: runStart, count: runCount});
+    runs.push({start: runStart, count: runCount, pattern: pattern});
   }
 
   return runs;
@@ -207,7 +202,7 @@ var findRuns = function(pattern, doc, delimiter) {
  * @return {string} newDoc - String whose runs have been compressed to one
  * instance of the repeated pattern.
  */
-var tagRuns = function(runs, pattern, doc, delimiter, showNum,
+var tagRuns = function(runs, doc, delimiter, showNum,
                        multiplier, tagStart, tagEnd) {
   var lastPos = 0;
   var newDoc = '';
@@ -215,12 +210,12 @@ var tagRuns = function(runs, pattern, doc, delimiter, showNum,
     newDoc += doc.slice(lastPos, run.start);
 
     if (showNum && run.count > 1) {
-      newDoc += tagStart + pattern + multiplier + run.count + tagEnd + delimiter;
+      newDoc += tagStart + run.pattern + multiplier + run.count + tagEnd + delimiter;
     } else {
-      newDoc += pattern + delimiter;
+      newDoc += run.pattern + delimiter;
     }
 
-    lastPos = run.start + run.count * (pattern.length + 1);
+    lastPos = run.start + run.count * (run.pattern.length + 1);
   }
 
   if (lastPos < doc.length) {
@@ -247,14 +242,17 @@ var tagRuns = function(runs, pattern, doc, delimiter, showNum,
  */
 var tagRepeats = function(pattern, doc, delimiter, showNum, multiplier, tagStart, tagEnd) {
   var runs = findRuns(pattern, doc, delimiter);
-  return tagRuns(runs, pattern, doc, delimiter, showNum, multiplier, tagStart, tagEnd);
+  return tagRuns(runs, doc, delimiter, showNum, multiplier, tagStart, tagEnd);
 };
 
-module.exports = tagRepeats;
+module.exports.tagRepeats = tagRepeats;
+module.exports.findRuns = findRuns;
+module.exports.tagRuns = tagRuns;
 
 },{"../lib/common.js":1}],"libhrc":[function(require,module,exports){
 var LCPArray = require('../lib/lcpArray.js');
-var tagRepeats = require('../lib/tagRepeats.js');
+var tagging = require('../lib/tagRepeats.js');
+var common = require('../lib/common.js');
 
 /**
  * Replace sequential repeats, runs, by one instance of the pattern.
@@ -276,15 +274,27 @@ var naive_compress = function(s,
                               left_tag,
                               right_tag) {
   var lcp_array = LCPArray(s, delimiter);
-  //console.log(lcp_array);
   if (lcp_array.length > 0) {
     // Starting from the longest LCP, find the first LCP that compresses the
     // original string.
+
+    // Sort the LCP array so we know we are going in order of length of common
+    // prefix.
+    lcp_array.sort(function(a, b) {
+      if (a.length < b.length) {
+        return -1;
+      } else if (a.length > b.length) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    
     var i = lcp_array.length - 1;
     var lrs = null;
     while (i >= 0) {
       var testlrs = lcp_array[i];
-      var testCompress = tagRepeats(testlrs, s, delimiter);
+      var testCompress = tagging.tagRepeats(testlrs, s, delimiter);
       if (testCompress.length < s.length) {
         lrs = testlrs;
         break;
@@ -293,7 +303,79 @@ var naive_compress = function(s,
     }
 
     if (lrs) {
-      return tagRepeats(lrs, s, delimiter, showNum, multiplier, left_tag, right_tag);
+      return tagging.tagRepeats(lrs, s, delimiter, showNum, multiplier, left_tag, right_tag);
+    } else {
+      return s;
+    }
+  } else {
+    return s;
+  }
+};
+
+var greedy_compress = function(s,
+                               delimiter,
+                               showNum,
+                               multiplier,
+                               left_tag,
+                               right_tag) {
+  var lcp_array = LCPArray(s, delimiter);
+
+  if (lcp_array.length > 0) {
+    // Sort the LCP array so we know we are going in order of length of common
+    // prefix.
+    lcp_array.sort(function(a, b) {
+      if (a.length < b.length) {
+        return -1;
+      } else if (a.length > b.length) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    var testStr = s;
+    var greedySet = [];
+    var i = lcp_array.length - 1;
+    while (i >= 0) {
+      var testlrs = lcp_array[i];
+      var runs = tagging.findRuns(testlrs, testStr, delimiter);
+      //console.log("'", runs, "'");
+      // Filter only runs of length greater than 1.
+      var newRuns = runs.filter(function(x) { return x.count > 1; });
+
+      // Filter runs that do not overlap with existing set.
+      var includedRuns = newRuns.filter(function(x) {
+        for (var i = 0; i < greedySet.length; i++) {
+          var r = greedySet[i];
+          var xEnd = x.start + x.pattern.length*x.count + (x.count - 1);
+          var rEnd = r.start + r.pattern.length*r.count + (r.count - 1);
+          //console.log(x.pattern, x.count, r.pattern, r.count, x.start, xEnd, r.start, rEnd);
+          if (x.start <= rEnd && r.start <= xEnd) {
+              return false;
+            }
+        }
+        return true;
+      });
+
+      // Use these runs to delete from string.
+      for (x of includedRuns) {
+        testStr = common.nullOutChars(
+          testStr, x.start, x.start + x.pattern.length*x.count + (x.count - 1), ' '
+        );
+      }
+
+      // Add to the set
+      greedySet.push.apply(greedySet, includedRuns);
+
+      i--;
+    }
+
+    if (greedySet.length > 0) {
+      // Sort set for easy tagging.
+      var sortedGreedySet = greedySet.sort(function(a, b) {
+        return a.start - b.start;
+      });
+      return tagging.tagRuns(sortedGreedySet, s, delimiter, showNum, multiplier, left_tag, right_tag);
     } else {
       return s;
     }
@@ -303,7 +385,8 @@ var naive_compress = function(s,
 };
 
 module.exports.LCPArray = LCPArray;
-module.exports.tagRepeatedPhrases = tagRepeats;
+module.exports.tagRepeatedPhrases = tagging.tagRepeats;
 module.exports.naive_compress = naive_compress;
+module.exports.greedy_compress = greedy_compress;
 
-},{"../lib/lcpArray.js":2,"../lib/tagRepeats.js":3}]},{},[]);
+},{"../lib/common.js":1,"../lib/lcpArray.js":2,"../lib/tagRepeats.js":3}]},{},[]);
