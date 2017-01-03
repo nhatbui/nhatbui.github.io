dc27d547ac0dc0a325712c7f07a779eca99000f5
Nhat Bui <nhatbui@Nhats-MBP.fios-router.home>
2017-01-02 21:22:21 -0800

    Add new version of libhrc library. Add ID for demo div to directly link

--- js/libhrc/libhrc.bundle.js
+++ js/libhrc/libhrc.bundle.js
@@ -31,6 +31,14 @@ var findAllIndicesRegex = function(str, re) {
   return indices;
 };
 
+/**
+ * Replace characters in range with another character.
+ * @param {string} str - string with a range of characters to be replaced.
+ * @param {int} start - position in string for start of range (inclusive).
+ * @param {int} end - position in string for end of range (exclusive).
+ * @param {string} nullChar - character to replace characters in range.
+ * @return {string} New string whose range have been replaced with instances of nullChar.
+ */
 var nullOutChars = function(str, start, end, nullChar) {
   return str.slice(0, start) + nullChar.repeat(end - start) + str.slice(end);
 }
@@ -91,7 +99,7 @@ var LCPArray = function(str, splitOn) {
   // Split the sentence into tokens.
   var tokenList = str.split(splitOn);
   if (tokenList.length <= 1) {
-    return '';
+    return [];
   }
 
   // Get positions of each token in the original string.
@@ -100,6 +108,7 @@ var LCPArray = function(str, splitOn) {
   // Create the shuffix array by sorting it.
   var shuffixArray = Array.apply(null, Array(tokenList.length))
     .map(function(_, i) {return i;});
+
   shuffixArray.sort(function(a, b) {
     var s1 = tokenList.slice(a);
     var s2 = tokenList.slice(b);
@@ -121,9 +130,7 @@ var LCPArray = function(str, splitOn) {
     var tokensMatched = longestCommonPrefix(sh1, sh2, tokenList);
     var lcp = tokenList.slice(sh1, sh1 + tokensMatched).join(splitOn);
 
-    if (lcp.length > 0) {
-      lcp_array.push(lcp);
-    }
+    lcp_array.push(lcp);
   }
 
   return lcp_array;
@@ -274,6 +281,8 @@ var naive_compress = function(s,
                               left_tag,
                               right_tag) {
   var lcp_array = LCPArray(s, delimiter);
+  lcp_array = lcp_array.filter(function (x) { return x.length > 0; });
+
   if (lcp_array.length > 0) {
     // Starting from the longest LCP, find the first LCP that compresses the
     // original string.
@@ -289,7 +298,7 @@ var naive_compress = function(s,
         return 0;
       }
     });
-    
+
     var i = lcp_array.length - 1;
     var lrs = null;
     while (i >= 0) {
@@ -312,6 +321,21 @@ var naive_compress = function(s,
   }
 };
 
+/**
+ * Replace sequential repeats, runs, by one instance of the pattern.
+ * Optionally, add styling to all reduced runs.
+ * Greedy heuristic where matches are checked starting at the longest common
+ * prefix.
+ * @param {string} s - String to compress (hopefully).
+ * @param {string} delimiter - String to split s into tokens.
+ * @param {boolean} showNum - Flag to show number of repeats.
+ * @param {string} multiplier - String to show before number of repeats.
+ * @param {string} left_tag - String to show before start of run.
+ * @param {string} right_tag - String to show at the end of run.
+ * @return {string} If repeats are found, return new string whose
+ * runs have been compressed to one instance of the repeated pattern. If not,
+ * return original string.
+ */
 var greedy_compress = function(s,
                                delimiter,
                                showNum,
@@ -319,59 +343,62 @@ var greedy_compress = function(s,
                                left_tag,
                                right_tag) {
   var lcp_array = LCPArray(s, delimiter);
+  lcp_array = lcp_array.filter(function (x) { return x.length > 0; });
 
   if (lcp_array.length > 0) {
-    // Sort the LCP array so we know we are going in order of length of common
-    // prefix.
-    lcp_array.sort(function(a, b) {
-      if (a.length < b.length) {
-        return -1;
-      } else if (a.length > b.length) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
+    // Estimate the savings of each LCP
+    var lcp_savings = [];
+    for (var i = 0; i < lcp_array.length; i++) {
+      // Find runs of testlrs.
+      var runs = tagging.findRuns(lcp_array[i], s, delimiter);
 
-    var testStr = s;
-    var greedySet = [];
-    var i = lcp_array.length - 1;
-    while (i >= 0) {
-      var testlrs = lcp_array[i];
-      var runs = tagging.findRuns(testlrs, testStr, delimiter);
-      //console.log("'", runs, "'");
       // Filter only runs of length greater than 1.
       var newRuns = runs.filter(function(x) { return x.count > 1; });
 
-      // Filter runs that do not overlap with existing set.
-      var includedRuns = newRuns.filter(function(x) {
-        for (var i = 0; i < greedySet.length; i++) {
-          var r = greedySet[i];
-          var xEnd = x.start + x.pattern.length*x.count + (x.count - 1);
-          var rEnd = r.start + r.pattern.length*r.count + (r.count - 1);
-          //console.log(x.pattern, x.count, r.pattern, r.count, x.start, xEnd, r.start, rEnd);
-          if (x.start <= rEnd && r.start <= xEnd) {
-              return false;
-            }
-        }
-        return true;
-      });
+      // Savings = (num_tokens_in_pattern) * (repeats - 1)
+      var savings = 0;
+      for (var j = 0; j < newRuns.length; j++) {
+        // Count the number of delimiters
+        var delimiterCount = common.findAllIndices(newRuns[j].pattern, ' ');
+        // Tokens = num_delimiters + 1
+        var tokensCount = delimiterCount.length + 1;
+        // Compute savings
+        savings += (newRuns[j].count - 1) * tokensCount;
+      }
+      lcp_savings.push({ string: lcp_array[i], savings: savings });
+    }
+
+    // Sort by ascending savings
+    lcp_savings.sort(function(a, b) {
+      return a.savings - b.savings;
+    });
+
+    // Check LCP in descending savings, starting with the largest.
+    var testStr = s;
+    var greedySet = [];
+    for (var i = lcp_savings.length - 1; i >= 0; i--) {
+      var testlrs = lcp_savings[i].string;
+
+      // Find runs of testlrs.
+      var runs = tagging.findRuns(testlrs, testStr, delimiter);
+
+      // Filter only runs of length greater than 1.
+      var newRuns = runs.filter(function(x) { return x.count > 1; });
 
       // Use these runs to delete from string.
-      for (x of includedRuns) {
+      // Deleting is important to find sebsequent runs of other LCS.
+      for (x of newRuns) {
         testStr = common.nullOutChars(
           testStr, x.start, x.start + x.pattern.length*x.count + (x.count - 1), ' '
         );
       }
 
       // Add to the set
-      greedySet.push.apply(greedySet, includedRuns);
-
-      i--;
+      greedySet.push.apply(greedySet, newRuns);
     }
 
     if (greedySet.length > 0) {
-      // Sort set for easy tagging.
+      // Sort set for tagging.
       var sortedGreedySet = greedySet.sort(function(a, b) {
         return a.start - b.start;
       });
